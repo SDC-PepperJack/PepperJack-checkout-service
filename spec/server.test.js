b3b7774cd671e044c98d5bb8282d84f9426a5ebb
Brian Kim <briaansjkim@gmail.com>
2019-10-31 17:17:22 -0400

    implemented passing delete request test

--- spec/server.test.js
+++ spec/server.test.js
@@ -43,14 +43,14 @@ describe('API Routes', () => {
     expect(response.response.data).toBe('Error adding new product');
   });
 
-  // test('A delete request to /api/checkout/:productId/details should return a success message', async() => {
-  //   try {
-  //     var response = await axios.delete('http://127.0.0.1:1234/api/checkout/1/details');
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
-  //   expect(response.status).toBe(200);
-  // });
+  test('A delete request to /api/checkout/:productId/details should return a success message', async() => {
+    try {
+      var response = await axios.delete('http://127.0.0.1:1234/api/checkout/1/details');
+    } catch (err) {
+      console.error(err);
+    }
+    expect(response.status).toBe(200);
+  });
 
   // test('A put request to /api/checkout/:productId/details should return a success message', async() => {
   //   try {
