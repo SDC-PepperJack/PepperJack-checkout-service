5dae3445ba4e49ae01ad6cf1536d2f1ce081fccf
Brian Kim <briaansjkim@gmail.com>
2019-11-01 17:12:52 -0400

    set up postgresql and created productDetails table in database

--- pgdb/index.js
+++ pgdb/index.js
@@ -8,10 +8,22 @@ const client = new Pool({
 });
 
 client.connect()
-  .then(() => console.log('connected!'));
+  .then(() => console.log('connected!'))
   .then(() => {
     client.query(`CREATE TABLE productDetails (
-
-    )`)
+      productId BIGSERIAL PRIMARY KEY,
+      sellerId BIGINT UNIQUE,
+      sellerName VARCHAR(50),
+      averageReviewScore BIGINT,
+      numberReviews BIGINT,
+      itemName VARCHAR(50),
+      badge VARCHAR(50),
+      itemPrice DECIMAL,
+      freeShipping BOOLEAN,
+      productOptions VARCHAR(50),
+      personalization BOOLEAN,
+      availableQuantity BIGINT,
+      onOrder INT
+    )`);
   });
 
