62728778f9890471002b8ad70fea455dfa922c89
Brian Kim <briaansjkim@gmail.com>
2019-11-02 19:52:36 -0400

    successfully generated 10 million records into a csv file

---
+++ pgDb/pgConnect.js
@@ -0,0 +1,29 @@
+const { Pool } = require('pg');
+
+const client = new Pool({
+  user: 'briankim',
+  host: 'localhost',
+  database: 'testing',
+  password: '',
+});
+
+client.connect()
+  .then(() => console.log('connected!'))
+  .then(() => {
+    client.query(`CREATE TABLE productDetails (
+      productId BIGSERIAL PRIMARY KEY,
+      sellerId BIGINT,
+      sellerName VARCHAR(50),
+      averageReviewScore BIGINT,
+      numberReviews BIGINT,
+      itemName VARCHAR(50),
+      badge VARCHAR(50),
+      itemPrice DECIMAL,
+      freeShipping BOOLEAN,
+      productOptions VARCHAR(50),
+      personalization BOOLEAN,
+      availableQuantity BIGINT,
+      onOrder INT
+    )`);
+  });
+
