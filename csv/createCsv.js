a168691bab2bbb217fbc4502744b9ecba0d89f70
Brian Kim <briaansjkim@gmail.com>
2019-11-11 12:00:10 -0500

    optimized db by separating 1 table into 11 tables with 6 csv files

--- csv/createCsv.js
+++ csv/createCsv.js
@@ -18,6 +18,7 @@ dataGenerator = new dataGenerator();
 //       let averageReviewScore = dataGenerator.generateAverageReviewScore();
 //       let numberReviews = dataGenerator.generateNumReviews();
 //       let itemName = dataGenerator.generateProductName();
+
 //       let itemPrice = dataGenerator.generateItemPrice(10, 301);
 //       let freeShipping = dataGenerator.generateBoolean();
 //       let personalization = dataGenerator.generateBoolean();
@@ -51,87 +52,145 @@ dataGenerator = new dataGenerator();
 // });
 
 // SIZES CSV
-let sizes = ["extra-small", "small", "medium", "large", "extra_large"];
-const writeUsers = fs.createWriteStream('pgProductSizes.csv');
-function writeSizes(writer, encoding, callback) {
-  let i = 10000000;
-  let id = 0;
-  function write() {
-    let ok = true;
-    do {
-      i -= 1;
-      id += 1; // size id
-      for (var j = 0; j < sizes.length; j++) {
-        let data = `${id}, ${j}\n`;
+// let sizes = ["extra-small", "small", "medium", "large", "extra_large"];
+// const writeUsers = fs.createWriteStream('pgProductSizes.csv');
+// function writeSizes(writer, encoding, callback) {
+//   let i = 10000000;
+//   let id = 0;
+//   function write() {
+//     let ok = true;
+//     do {
+//       i -= 1;
+//       id += 1; // size id
+//       for (var j = 0; j < sizes.length; j++) {
+//         let data = `${id}, ${j}\n`;
 
-        if ( i === 0) {
-          // i has reached 0, so write the data invoke writeUsers.end()
-          writer.write(data, encoding, callback);
-        } else {
-          // i hasn't reached 0 yet, so write the data and continue
-          // don't pass in the callback
-          ok = writer.write(data, encoding);
-        }
+//         if ( i === 0) {
+//           // i has reached 0, so write the data invoke writeUsers.end()
+//           writer.write(data, encoding, callback);
+//         } else {
+//           // i hasn't reached 0 yet, so write the data and continue
+//           // don't pass in the callback
+//           ok = writer.write(data, encoding);
+//         }
 
-      }
+//       }
 
-    } while (i > 0 && ok);
-    if (i >0) {
-      // pauses the write process when buffer is full
-      // once drain is fired, process continues until all records have been written
-        // drain event is for when a writable stream's internal buffer has been emptied
-          // when highWaterMark prop (max bytes of data that can be stored inside internal buffer) is exceeded
-      writer.once('drain', write);
-    }
-  }
-  write();
-}
+//     } while (i > 0 && ok);
+//     if (i >0) {
+//       // pauses the write process when buffer is full
+//       // once drain is fired, process continues until all records have been written
+//         // drain event is for when a writable stream's internal buffer has been emptied
+//           // when highWaterMark prop (max bytes of data that can be stored inside internal buffer) is exceeded
+//       writer.once('drain', write);
+//     }
+//   }
+//   write();
+// }
 
-writeSizes(writeUsers, 'utf-8', () => {
-  writeUsers.end();
-});
+// writeSizes(writeUsers, 'utf-8', () => {
+//   writeUsers.end();
+// });
 
 // MATERIALS CSV
-const writeUsers = fs.createWriteStream('pgProductMaterials.csv');
-let materials = ['ash', 'walnut', 'ebony', 'aluminum', 'brushed_steel', 'glass', 'solid_titanium', 'pure_gold', 'solid_diamond'];
-function writeMaterials(writer, encoding, callback) {
-  let i = 10000000;
-  let id = 0;
-  function write() {
-    let ok = true;
-    do {
-      i -= 1;
-      id += 1;
-      for (var j = 0; j < materials.length; j++) {
-        let data = `${id}, ${j}\n`;
-          if ( i === 0) {
-            // i has reached 0, so write the data invoke writeUsers.end()
-            writer.write(data, encoding, callback);
-          } else {
-            // i hasn't reached 0 yet, so write the data and continue
-            // don't pass in the callback
-            ok = writer.write(data, encoding);
-          }
-      }
-    } while (i > 0 && ok);
-    if (i >0) {
-      // pauses the write process when buffer is full
-      // once drain is fired, process continues until all records have been written
-        // drain event is for when a writable stream's internal buffer has been emptied
-          // when highWaterMark prop (max bytes of data that can be stored inside internal buffer) is exceeded
-      writer.once('drain', write);
-    }
-  }
-  write();
-}
+// const writeUsers = fs.createWriteStream('pgProductMaterials.csv');
+// let materials = ['ash', 'walnut', 'ebony', 'aluminum', 'brushed_steel', 'glass', 'solid_titanium', 'pure_gold', 'solid_diamond'];
+// function writeMaterials(writer, encoding, callback) {
+//   let i = 10000000;
+//   let id = 0;
+//   function write() {
+//     let ok = true;
+//     do {
+//       i -= 1;
+//       id += 1;
+//       for (var j = 0; j < materials.length; j++) {
+//         let data = `${id}, ${j}\n`;
+//           if ( i === 0) {
+//             writer.write(data, encoding, callback);
+//           } else {
+//             ok = writer.write(data, encoding);
+//           }
+//       }
+//     } while (i > 0 && ok);
+//     if (i >0) {
+//       writer.once('drain', write);
+//     }
+//   }
+//   write();
+// }
 
-writeMaterials(writeUsers, 'utf-8', () => {
-  writeUsers.end();
-});
+// writeMaterials(writeUsers, 'utf-8', () => {
+//   writeUsers.end();
+// });
 
-// PATTERNS CSV
+// // PATTERNS CSV
 // let patterns = ['checkboard', 'argile', 'striped_vertical', 'striped_horizontal', 'stars', 'bars', 'tie_dye'];
-function writePatterns(writer, encoding, callback) {
+// const writeUsers = fs.createWriteStream('pgProductPatterns.csv');
+// function writePatterns(writer, encoding, callback) {
+//   let i = 10000000;
+//   let id = 0;
+//   function write() {
+//     let ok = true;
+//     do {
+//       i -= 1;
+//       id += 1;
+//       for (var j = 0; j < patterns.length; j++) {
+//         let data = `${id}, ${j}\n`;
+//         if ( i === 0) {
+//           writer.write(data, encoding, callback);
+//         } else {
+//           ok = writer.write(data, encoding);
+//         }
+//       }
+
+//     } while (i > 0 && ok);
+//     if (i >0) {
+//       writer.once('drain', write);
+//     }
+//   }
+//   write();
+// }
+
+// writePatterns(writeUsers, 'utf-8', () => {
+//   writeUsers.end();
+// });
+
+// // FONTS
+// let fonts = ['serif', 'comic_sans', 'typewriter', 'cursive', 'star_wars'];
+// const writeUsers = fs.createWriteStream('pgProductFonts.csv');
+// function writeFonts(writer, encoding, callback) {
+//   let i = 10000000;
+//   let id = 0;
+//   function write() {
+//     let ok = true;
+//     do {
+//       i -= 1;
+//       id += 1;
+//       for (var j = 0; j < fonts.length; j++) {
+//         let data = `${id}, ${j}\n`;
+//         if ( i === 0) {
+//           writer.write(data, encoding, callback);
+//         } else {
+//           ok = writer.write(data, encoding);
+//         }
+//       }
+
+//     } while (i > 0 && ok);
+//     if (i >0) {
+//       writer.once('drain', write);
+//     }
+//   }
+//   write();
+// }
+
+// writeFonts(writeUsers, 'utf-8', () => {
+//   writeUsers.end();
+// });
+
+// BADGES
+let badges = ['Bestseller','Badseller', null];
+const writeUsers = fs.createWriteStream('pgProductBadges.csv');
+function writeBadges(writer, encoding, callback) {
   let i = 10000000;
   let id = 0;
   function write() {
@@ -139,68 +198,24 @@ function writePatterns(writer, encoding, callback) {
     do {
       i -= 1;
       id += 1;
-      for (var j = 0; j < patterns.length; j++) {
+      for (var j = 0; j < badges.length; j++) {
         let data = `${id}, ${j}\n`;
         if ( i === 0) {
-          // i has reached 0, so write the data invoke writeUsers.end()
           writer.write(data, encoding, callback);
         } else {
-          // i hasn't reached 0 yet, so write the data and continue
-          // don't pass in the callback
           ok = writer.write(data, encoding);
         }
       }
 
     } while (i > 0 && ok);
     if (i >0) {
-      // pauses the write process when buffer is full
-      // once drain is fired, process continues until all records have been written
-        // drain event is for when a writable stream's internal buffer has been emptied
-          // when highWaterMark prop (max bytes of data that can be stored inside internal buffer) is exceeded
       writer.once('drain', write);
     }
   }
   write();
 }
 
-writePatterns(writeUsers, 'utf-8', () => {
+writeBadges(writeUsers, 'utf-8', () => {
   writeUsers.end();
 });
 
-// FONTS
-let fonts = ['serif', 'comic_sans', 'typewriter', 'cursive', 'star_wars'];
-function writeFonts(writer, encoding, callback) {
-  let i = 10000000;
-  let id = 0;
-  function write() {
-    let ok = true;
-    do {
-      i -= 1;
-      id += 1;
-      for (var j = 0; j < fonts.length; j++) {
-        let data = `${id}, ${j}\n`;
-        if ( i === 0) {
-          // i has reached 0, so write the data invoke writeUsers.end()
-          writer.write(data, encoding, callback);
-        } else {
-          // i hasn't reached 0 yet, so write the data and continue
-          // don't pass in the callback
-          ok = writer.write(data, encoding);
-        }
-      }
-
-    } while (i > 0 && ok);
-    if (i >0) {
-      // pauses the write process when buffer is full
-      // once drain is fired, process continues until all records have been written
-        // drain event is for when a writable stream's internal buffer has been emptied
-          // when highWaterMark prop (max bytes of data that can be stored inside internal buffer) is exceeded
-      writer.once('drain', write);
-    }
-  }
-  write();
-}
-
-writeFonts(writeUsers, 'utf-8', () => {
-  writeUsers.end();
-});
--- pgDb/pgSeed.js
+++ pgDb/pgSeed.js
@@ -9,10 +9,20 @@ const pool = new Pool({
   password: '',
 });
 
-const file = path.join(__dirname, '/csvFile/pgProductDetail.csv');
+const fileProduct = path.join(__dirname, '/csvFile/pgProductDetail.csv');
+const fileSizes = path.join(__dirname, '/csvFile/pgProductSizes.csv');
+const fileMaterials = path.join(__dirname, '/csvFile/pgProductMaterials.csv');
+const filePatterns = path.join(__dirname, '/csvFile/pgProductPatterns.csv');
+const fileFonts = path.join(__dirname, '/csvFile/pgProductFonts.csv');
+const fileBadges = path.join(__dirname, '/csvFile/pgProductBadges.csv');
 
 pool.connect()
   .then(() => console.log('Ready to insert'))
-  .then(() => pool.query(`COPY productdetails FROM '${file}' CSV`))
+  .then(() => pool.query(`COPY productdetails FROM '${fileProduct}' CSV`))
+  .then(() => pool.query(`COPY productsize FROM '${fileSizes}' CSV`))
+  .then(() => pool.query(`COPY productmaterial FROM '${fileMaterials}' CSV`))
+  .then(() => pool.query(`COPY productpattern FROM '${filePatterns}' CSV`))
+  .then(() => pool.query(`COPY productfonts FROM '${fileFonts}' CSV`))
+  .then(() => pool.query(`COPY productbadges FROM '${fileBadges}' CSV`))
   .then(() => console.log('Data imported'))
   .catch((err) => console.log('Error', err))
