8083c8656d177bef81e65697369da2b00b700a4e
Brian Kim <briaansjkim@gmail.com>
2019-11-14 14:37:59 -0500

    added missing column

--- pgDb/pgConnect.js
+++ pgDb/pgConnect.js
@@ -1,15 +1,31 @@
-CREATE TABLE productDetails(
-  productId SERIAL PRIMARY KEY NOT NULL,
-  sellerId INT NOT NULL,
-  sellerName VARCHAR(50),
-  averageReviewScore INT NOT NULL,
-  numberReviews INT NOT NULL,
-  itemName VARCHAR(50) NOT NULL,
-  badge VARCHAR(50) NOT NULL,
-  itemPrice DECIMAL NOT NULL,
-  freeShipping BOOLEAN,
-  availableQuantity INT NOT NULL,
-  productOptions character[]/ JSON,
-)
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'briankim',
+  host: 'localhost',
+  database: 'btetsy',
+  password: '',
+});
+
+pool.connect()
+  .then(() => console.log('Ready to create'))
+  .then(() => pool.query(`
+    CREATE TABLE productDetails(
+      productId SERIAL PRIMARY KEY NOT NULL,
+      sellerId INT NOT NULL,
+      sellerName VARCHAR(50),
+      averageReviewScore INT NOT NULL,
+      numberReviews INT NOT NULL,
+      itemName VARCHAR(50) NOT NULL,
+      itemPrice DECIMAL NOT NULL,
+      badge VARCHAR(50) NOT NULL,
+      freeShipping BOOLEAN,
+      productOptions text,
+      personalization BOOLEAN,
+      availableQuantity INT NOT NULL,
+      onOrder INT NOT NULL
+    )
+  `))
+  .then(() => console.log('Table created'));
 
 // CREATE UNIQUE INDEX ON productId
